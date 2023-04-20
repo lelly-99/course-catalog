f700a77205d9529c9d9518e912b9db1d765b0436
progress <leswifil@gmail.com>
2023-04-20 16:24:34 +0200

    updated

--- course-cost/test/cost.test.js
+++ course-cost/test/cost.test.js
@@ -1,10 +1,20 @@
+//add a description of what the function should do
+
 describe("courseCost function", function() {
 
-    it("should be able to return valid onject", function(){
+    it("should be able to return ", function(){
+        //create variables storing and one with code
         let dateOne = "2023-05-21"
         let codeOne = "r1"
-        
-       
-   
+        //create a object to test
+        results =  {
+            status : "success",
+            code: "r1",
+            description : course.courseName,
+            cost : course.cost,
+            discount :  
+            amountDue : 
+        }
+        assert.equal(results, tutorWageCalculator(dateOne, codeOne))
 })
 
