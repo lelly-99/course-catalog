ec247ceed5d6f7de963a3b83dbcc326025ac9444
progress <leswifil@gmail.com>
2023-04-20 16:18:44 +0200

    update

---
+++ course-cost/test/cost.test.js
@@ -0,0 +1,10 @@
+describe("courseCost function", function() {
+
+    it("should be able to return valid onject", function(){
+        let dateOne = "2023-05-21"
+        let codeOne = "r1"
+        
+       
+   
+})
+
---
+++ tutor-wages/tutorWageCalculator.js
@@ -0,0 +1,12 @@
+//Write a tutorWageCalculator which calculates how much a tutor will get paid based on their hourly rate and hours they have worked 
+
+//Use timesheetString and level as parameters
+//Split the string with number separated by -
+//Calculate sum of numbers in a string
+//Use conditional statement
+//If tutor is level 1 , multiply the the string with 75
+//Else If turor is level 2 multiply by 90 
+
+//Else if tutor is level 3 multiply by 105 
+
+//Else return invalid timeseets
